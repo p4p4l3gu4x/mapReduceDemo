18948c39890c270483fe4721b0acf819f97a32fc
Hudson Martins <p4p4l3gu4x@gmail.com>
2017-07-07 00:07:46 -0300

    Compartilhando o Projeto.

---
+++ mapReduceDemo/src/main/java/net/hudson/MapReduceDemoApplication.java
@@ -0,0 +1,12 @@
+package net.hudson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapReduceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapReduceDemoApplication.class, args);
+	}
+}
